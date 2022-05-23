48d23809ba2ef275662fc01af5bab31fe98d0dce
shivamtg <shivamtg1376@gmail.com>
2022-05-23 16:58:11 +0200

    display id and name

--- script.js
+++ script.js
@@ -30,7 +30,10 @@
         let colImg = createCol();
         let imgTag = createImg(detailArr.sprites.other.home.front_default);
         colImg.appendChild(imgTag);
+        let colDetails = createCol();
+        colDetails = addDetails(colDetails, detailArr);
         row.appendChild(colImg);
+        row.appendChild(colDetails);
         parent.appendChild(row);
         detailRowCreation = true;
     }
@@ -42,8 +45,7 @@
 
     const createRow = () => {
         let row = document.createElement('div');
-        row.setAttribute('class', 'row align-items-center');
-        row.setAttribute('id' , 'pokeDetails')
+        row.setAttribute('class', 'row align-items-center pokeDetails');
         return row;
     }
     const createCol = () => {
@@ -58,5 +60,18 @@
         img.setAttribute('class', 'shadow');
         img.height = 250;
         return img;
+    }
+    const addDetails = (col , arr) => {
+        col.setAttribute('id', 'details');
+        col.classList.add('shadow');
+        let id = arr.id;
+        let name = arr.name;
+        let h1 = document.createElement('h1');
+        h1.innerHTML = `Name : ${name.toUpperCase()}`
+        let h2 = document.createElement('h2');
+        h2.innerHTML = id;
+        col.appendChild(h1);
+        col.appendChild(h2);
+        return col;
     }
 })();
