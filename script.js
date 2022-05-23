525e181f7198fb91f0356f86030ef17ba0a61626
shivamtg <shivamtg1376@gmail.com>
2022-05-23 14:27:32 +0200

    fetch method edited, fetch by id or name only

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
 
 
 (()=>{
-    fetchPokemons();
-    let detailRowCreation = false ;
-    /*______________Async Functions______________*/
-    async function fetchPokemons () {
-        let data = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
-        let pokeArr = await data.json();
-        console.log(pokeArr);
-         main(pokeArr.results);
-    }
-    async function fetchInfoPoke (url) {
-        let info = await fetch(`${url}`);
-        let detailArr = await info.json();
-        console.log(detailArr);
-        displayPokeDetail(detailArr);
-    }
-    /*______________Other Functions______________*/
-    const main = (pokeArr) => {
-        document.getElementById('searchBtn').addEventListener('click',()=>{
-            searchByName(pokeArr);
-        })
 
+    let detailRowCreation = false;
+
+    document.getElementById('searchBtn').addEventListener('click' , ()=>{
+        let pokemon = document.getElementById('pokeName').value ;
+        fetchPokemons(pokemon);
+    })
+
+    /*______________Async Functions______________*/
+
+    async function fetchPokemons (pokemon) {
+        let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        let foundPokemon = await data.json();
+        console.log(foundPokemon);
+        displayPokeDetail(foundPokemon);
     }
-    const searchByName = (pokeArr) => {
-        let pokToFind = document.getElementById('pokeName').value;
-        //console.log(pokToFind)
-        let found = pokeArr.find( el => el.name == pokToFind );
-        fetchInfoPoke(found.url);
-    }
+
+    /*______________Other Functions______________*/
+
 
     const displayPokeDetail = (detailArr) => {
         if (detailRowCreation){
