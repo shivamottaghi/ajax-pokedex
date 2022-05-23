97472f1b09272e89908cdd9d804f602a4d917320
shivamtg <shivamtg1376@gmail.com>
2022-05-23 11:24:54 +0200

    js get pokemon name, fetch, log details into console

--- script.js
+++ script.js
@@ -2,13 +2,31 @@
 
 (()=>{
     fetchPokemons();
+    /*______________Async Functions______________*/
     async function fetchPokemons () {
         let data = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
         let pokeArr = await data.json();
         console.log(pokeArr);
+         main(pokeArr.results);
     }
+    async function fetchInfoPoke (url) {
+        let info = await fetch(`${url}`);
+        let detailArr = await info.json();
+        console.log(detailArr);
+    }
+    /*______________Other Functions______________*/
+    const main = (pokeArr) => {
+        document.getElementById('searchBtn').addEventListener('click',()=>{
+            searchByName(pokeArr);
+        })
 
-
+    }
+    const searchByName = (pokeArr) => {
+        let pokToFind = document.getElementById('pokeName').value;
+        //console.log(pokToFind)
+        let found = pokeArr.find( el => el.name == pokToFind );
+        fetchInfoPoke(found.url);
+    }
 
 
 
