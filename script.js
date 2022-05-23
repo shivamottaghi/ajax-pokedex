4cf0f71f751ca437cf04f1cc8e8d5f1a4e0c6d51
shivamtg <shivamtg1376@gmail.com>
2022-05-23 11:59:14 +0200

    gets the name and display the picture of the pokemon

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
         let info = await fetch(`${url}`);
         let detailArr = await info.json();
         console.log(detailArr);
+        displayPokeDetail(detailArr);
     }
     /*______________Other Functions______________*/
     const main = (pokeArr) => {
@@ -28,6 +29,22 @@
         fetchInfoPoke(found.url);
     }
 
+    const displayPokeDetail = (detailArr) => {
+        //removeExtraRows();
+        let parent = document.querySelector('.container');
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row align-items-center');
+        row.setAttribute('id' , 'pokedetails')
+        let colImg = document.createElement('div');
+        colImg.setAttribute('class', 'col-12 col-md-3');
+        let imgTag = document.createElement('img');
+        imgTag.setAttribute('src', detailArr.sprites.front_default);
+        colImg.appendChild(imgTag);
+        row.appendChild(colImg);
+        parent.appendChild(row);
+    }
 
-
+    /*const removeExtraRows = () => {
+        let detailRow = document
+    }*/
 })();
